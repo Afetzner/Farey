23948d0f102b7509778d09b5fa6ab50d6d2352b5
Afetzner <59979332+Afetzner@users.noreply.github.com>
2024-02-13 07:29:46 -0500

    Added webpack

---
+++ frontend/src/index.ts
@@ -0,0 +1,21 @@
+import * as PIXI from 'pixijs';
+
+let app = new PIXI.Application({ width: 640, height: 360 });
+// @ts-ignore
+document.body.appendChild(app.view);
+
+// Magically load the PNG asynchronously
+let sprite = PIXI.Sprite.from('sprite.png');
+app.stage.addChild(sprite);
+
+// Add a variable to count up the seconds our demo has been running
+let elapsed = 0.0;
+// Tell our application's ticker to run a new callback every frame, passing
+// in the amount of time that has passed since the last tick
+app.ticker.add((delta : number) => {
+    // Add the time to our total elapsed time
+    elapsed += delta;
+    // Update the sprite's X position based on the cosine of our elapsed time.  We divide
+    // by 50 to slow the animation down a bit...
+    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
